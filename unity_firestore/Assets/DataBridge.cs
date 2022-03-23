bd5c6e5a320624034013067410fd7fac6eb0b6ba
Mostafa Saad <47724391+MostafaSaad7@users.noreply.github.com>
2022-03-23 13:54:06 +0200

    listener registration

--- unity_firestore/Assets/DataBridge.cs
+++ unity_firestore/Assets/DataBridge.cs
@@ -3,38 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Firebase.Firestore;
+using Firebase;
+using UnityEngine.Assertions;
+using Firebase.Extensions;
 
 public class DataBridge : MonoBehaviour
 {
 
-    public InputField name;
-    public InputField pass;
-    private string childPath = "Children/one cool Dude";
+    [SerializeField]
+    public InputField nameFeild;
+    public Text namef;
+    public Text passf;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    public InputField pass;
+    private string childPath = "Children/one_cool_Dude";
+    private ListenerRegistration listenerRegistration;
+
     void Start()
     {
-        
+         var firestore = FirebaseFirestore.DefaultInstance;
+            listenerRegistration =firestore.Document(childPath).Listen(snapshot=>{
+            var childData = snapshot.ConvertTo<Child>();
+            namef.text = childData.name;
+            passf.text = childData.password;
+
+                 });
+
+
     }
 
-    // Update is called once per frame
+      void OnDestroy() {
+
+    listenerRegistration.Stop();
+    }
+
     void Update()
     {
         
     }
 
     public void saveData()
-    {
+    {   var firestore = FirebaseFirestore.DefaultInstance;
         var childData = new Child
         {
-            name = name.text,
+        name = nameFeild.text,
         password = pass.text
-
         };
-  
-
-        var firestore = FirebaseFirestore.DefaultInstance;
         firestore.Document(childPath).SetAsync(childData);
-
     }
+
+
+    // public void loadData()
+    // {
+
+    //     firestore.Document(childPath).GetSnapshotAsync().ContinueWithOnMainThread((task =>
+    //     {
+    //         Assert.IsNull(task.Exception);
+
+    //         var childData = task.Result.ConvertTo<Child>();
+    //         nameFeild.text = childData.name;
+    //         pass.text = childData.password;
+
+    //         print("Name  "+ childData.name);
+    //         print("pass  "+ childData.name);
+    //     }
+    //     ));
+    // }
 }
